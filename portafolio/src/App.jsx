6dd6d96161ead0bf6cb9c2ead207417b94022df9
mely-0 <3145756813mela@gmail.com>
2022-12-30 17:13:49 -0500

    first commit

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,12 @@
+import { Portafolio } from "./components/portafolio"
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <Portafolio/>
+    </div>
+  )
+}
+
+export default App
---
+++ portafolio/src/components/Portafolio.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import '../components/portafolio.css'
+import image from '../components/img/html2.png'
+import image2 from '../components/img/css2.png'
+import image3 from '../components/img/js2.png'
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
+export const Portafolio = () => {
+  return (
+    <div className='portafolio'>
+    <div className='navegacion'>
+      <nav>
+        <ul>
+          <li>portafolio</li>
+          <li>sobremi</li>
+          <li>habilidades</li>
+          <li>proyectos</li>
+          <li>contactos</li>
+        </ul>
+      </nav>
+    </div>
+    <div className='contenedor1'>
+      <section className='texto'>
+        <h1>Franco Muñoz</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, cupiditate.</p>
+      </section>
+      <section className='foto'></section>
+    </div>
+    <section className='picture'></section>
+
+    <div className='sobremi'>
+      <h2>HELLO PEOPLE</h2>
+      <h4 className='aboutme'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, nobis. Accusantium hic expedita dolorem eos, voluptates illo modi ducimus adipisci labore aperiam, cum consectetur fugit blanditiis porro accusamus ipsa sequi!</h4>
+      <div className='habilidades'>
+        <section className='habi1'>
+          <img className='html' src={image}/>
+        </section>
+        <section className='habi2'>
+        <img className='css' src={image2}/>
+        </section>
+        <section className='habi3'>
+        <img className='js' src={image3}/>
+        </section>
+      </div>
+    </div>
+    <div className='container-pro'>
+      <article className='tituloProyecto'>
+      <h3>Proyectos</h3>
+      </article>
+    <div className="container">
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>
+                    <h3>Design</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"/>
+                    <h3>Code</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"/>
+                    <h3>Launch</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div className='contactos-pro'>
+    <article className='TituloContacto'>
+      <h3>contactanos</h3>
+      </article>
+    <div className='contactos'>
+      <article className='img-1'>
+      <FaGithub className='ico-1'/>
+      </article>
+      <article className='img-2'>
+        <FaFacebookSquare className='ico-2'/>
+      </article>
+      <article className='img-3'>
+        <HiMail className='ico-3'/>
+      </article>
+    </div>
+    </div>
+  </div>
+  )
+}
